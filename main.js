dd8d3ae8ffd8a170ffae16281b73848c7f70a21e
asoolito <roblox890king890@gmail.com>
2024-06-05 11:31:56 +0500

    %0y1

---
+++ main.js
@@ -0,0 +1,26 @@
+let userNum = prompt("1dan7gacha son kiriting");
+if(userNum == "1"){
+    let user1day = alert("dushanba")
+}
+else if(userNum == "2"){
+    let user2day = alert("seshanba")
+}
+else if(userNum == "3"){
+    let user3day = alert("chorshanba")
+}
+else if(userNum == "4"){
+    let user4day = alert("payshanba")
+}
+else if (userNum == "5"){
+    let user5day = alert("juma")
+}
+else if (userNum == "6"){
+    let user6day = alert("shanba")
+}
+else if (userNum == "7"){
+    let user7day = alert("yakshanba")
+}
+
+else{
+    alert("haftada 7kun bor")
+}
